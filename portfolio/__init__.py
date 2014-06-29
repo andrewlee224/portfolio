2083f8b9a95fd161631544079e858d01e4678039
Andrzej Lichaczewski <andrewlee224@gmail.com>
2014-06-29 21:15:53 +0200

    Controllers and templates for projects subpages

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -8,9 +8,15 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
 
-    config.add_route('home', '/')
+    config.add_route('projects', '/projects/')
     config.add_route('about', '/about/')
     config.add_route('contact', '/contact/')
+
+    # Projects subpages
+    config.add_route('rnapairs', '/projects/rnapairs/')
+    config.add_route('signalmonitor', '/projects/signalmonitor/')
+    config.add_route('drugscheduler', '/projects/drugscheduler/')
+    config.add_route('usgviewer', '/projects/usgviewer/')
 
     config.scan()
     return config.make_wsgi_app()
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,18 @@
-from pyramid.view import view_config
+from pyramid.view import view_config, notfound_view_config
+from pyramid.httpexceptions import HTTPNotFound
 
-
-@view_config(route_name='home', renderer='templates/index.jinja2')
-def my_view(request):
+@view_config(route_name='projects', renderer='templates/projects.jinja2')
+def index(request):
     return {'project': 'Portfolio'}
     
 @view_config(route_name='about', renderer='templates/about.jinja2')
 def about(request):
     return {}
+
+@view_config(route_name='rnapairs', renderer='templates/rnapairs.jinja2')
+def rnapairs(request):
+    return {}
+
+@notfound_view_config(append_slash=True)
+def notFound(request):
+    return HTTPNotFound("404 Error. Sorry, page not found.")
